b394198fa4ebb88d3170a48f1562ad476f3f0bc4
Fabio Castagna <fabiocastagna04@gmail.com>
2024-11-04 16:05:20 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,17 +1,41 @@
-function preload() {
-  // put preload code here
-}
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
-  const message =
-    "This is a template repository\nfor the course Laboratorio di Computergrafica\nCommunication Design, Politecnico di Milano";
-  textAlign(CENTER, CENTER);
-  textSize(16);
-  text(message, width / 2, height / 2);
+  noLoop();
 }
 
 function draw() {
-  // put drawing code here
+  background(255);
+  let cols = 10; // Numero di colonne
+  let rows = 5;  // Numero di righe
+  let padding = 100; // Padding intorno ai glifi
+  let glyphSize = (width - padding * (cols + 1)) / cols; // Calcolo della dimensione di ogni glifo
+
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let x = padding + i * (glyphSize + padding) + glyphSize / 2;
+      let y = padding + j * (glyphSize + padding) + glyphSize / 2;
+      drawGlyph(x, y, glyphSize);
+    }
+  }
+}
+
+function drawGlyph(x, y, size) {
+  push();
+  translate(x, y);
+  strokeWeight(1.5);
+  stroke(0);
+  noFill();
+  beginShape();
+  for (let i = 0; i < TWO_PI; i += random(0.2, 0.5)) {
+    let r = random(size * 0.5, size);
+    let x = r * cos(i);
+    let y = r * sin(i);
+    vertex(x, y);
+  }
+  endShape(CLOSE);
+  pop();
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
